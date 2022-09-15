5cbcabba74489aad74efbbf7adc2017e7780d696
Kato <gabriel.kato22@uol.com.br>
2022-09-15 19:12:30 -0300

    updated files

--- casinoExample01.cpp
+++ casinoExample01.cpp
@@ -13,6 +13,9 @@ int main()
 	int aposta1 = 0;
 	int aposta2 = 0;
 	int aposta3 = 0;
+	int apostasCorretas = 0;
+	int statsSpent = 0;
+	int statsQuantidadeApostas = 0;
 	char manterAposta = 'a';
 	char restartGame = 'a';
 	srand(time(NULL));
@@ -54,6 +57,7 @@ int main()
 		case 's':
 		//	cout << "sim";
 			balance = balance - valorAposta;
+			statsSpent = statsSpent + valorAposta;
 			break;
 		case 'n':
             goto resume01;
@@ -63,9 +67,14 @@ int main()
 		}
 
 	}
+	if (valorAposta <= 0)
+	{
+		cout << "como o valor da aposta foi menor ou igual a zero, voce podera jogar mas sem valer nada!" << endl;
+	}
 	else
 	{
 		balance = balance - valorAposta;
+		statsSpent = statsSpent + valorAposta;
 //		cout << "DEBUG! balance: " << balance << "valor aposta: " << valorAposta << endl;
 	}
 	
@@ -75,6 +84,7 @@ retryAposta:
 	cin.clear();
 	cin.ignore();
 	cin >> aposta1 >> aposta2 >> aposta3;
+	statsQuantidadeApostas = statsQuantidadeApostas + 3;
 	cout << endl;
 
 	/*
@@ -87,7 +97,7 @@ retryAposta:
 		goto retryAposta;
 	}
 	nota: nao funcionando atm, algum integer fica maior do que 10 por algum motivo o qual estou com bastante sono pra ir atras xd
-	*/
+	nota2: pode ser que nao esteja funcionando por causa que nao esta em um loop while :D
 
 	if (aposta1 || aposta2 || aposta3 > 10)
 	{
@@ -97,12 +107,14 @@ retryAposta:
 		aposta3 = 0;
 		goto retryAposta;
 	}
+	*/
 
-	//colocar codigo de add money nos if das apostas (done, agr e so testar e ver se ta pika)
+	// DONE! colocar codigo de add money nos if das apostas (done, agr e so testar e ver se ta pika)
 	if (aposta1 == numeroRandom1)
 	{
 		cout << "acertaste a primeira aposta!" << endl;
         balance = balance + valorAposta*2;
+		apostasCorretas = apostasCorretas + 1;
 	}
 	if (aposta1 != numeroRandom1)
 	{
@@ -114,6 +126,7 @@ retryAposta:
 	{
 		cout << "acertaste a segunda aposta!" << endl;
         balance = balance + valorAposta*2;
+		apostasCorretas = apostasCorretas + 1;
 	}
 	if (aposta2 != numeroRandom2)
 	{
@@ -125,6 +138,7 @@ retryAposta:
 	{
 		cout << "acertaste a terceira aposta!" << endl;
         balance = balance + valorAposta * 2;
+		apostasCorretas = apostasCorretas + 1;
 	}
 	if (aposta3 != numeroRandom3)
 	{
@@ -132,9 +146,28 @@ retryAposta:
      //   balance = balance - valorAposta;
 	}
 
-	// 2do: colocar dependendo de quantas apostas foram acertadas, textos diferentes de tryagain
+	// 2do (DONE!): colocar dependendo de quantas apostas foram acertadas, textos diferentes de tryagain
 	cout << endl;
-	cout << "bem jogado, jogue novamente!" << endl;
+	if (apostasCorretas == 0)
+	{
+		cout << "boa sorte na proxima! tente novamente" << endl;
+	}
+	if (apostasCorretas == 1)
+	{
+		cout << "bem jogado, jogue novamente!" << endl;
+		apostasCorretas = 0;
+	}
+	if (apostasCorretas == 2)
+	{
+		cout << "otima jogada! jogue novamente!" << endl;
+		apostasCorretas = 0;
+	}
+	if (apostasCorretas == 3)
+	{
+		cout << "parabens! voce acertou todas as apostas, use bem os " << balance + valorAposta * 6 << "! jogue novamente!" << endl;
+		apostasCorretas = 0;
+	}
+
 	cout << "sua carteira vale: $" << balance << endl;
 	cout << endl;
     
@@ -142,7 +175,9 @@ retryAposta:
 	{
 		tryAgain02:
 		cout << "o seu saldo ficou menor do que $1, portanto perdeste o jogo :(" << endl;
-		cout << "deseja recomecar o jogo?" << endl << "insira 's' para recomecar e insira 'n' para fechar o jogo." << endl;
+		cout << "deseja recomecar o jogo?" << endl << "insira 's' para recomecar e insira 'n' para fechar o jogo." << endl << "insira 'e' para ver as estatisticas." << endl;
+		cin.ignore();
+		cin.clear();
 		cin >> restartGame;
 		switch (restartGame)
 		{
@@ -158,8 +193,13 @@ retryAposta:
 		case 'n':
 			cout << "obrigado por jogar! ate a proxima!" << endl;
 			return 777;
+		case 'e':
+			cout << endl << "o total gasto nessa sessao foi: $" << statsSpent << endl;
+			cout << "voce realizou " << statsQuantidadeApostas << " apostas nessa sessao" << endl;
+			cout << "obrigado por jogar! ate a proxima!" << endl;
+			return 777;
 		default:
-			cout << "input invalido, insira 's' para recomecar e 'n' para sair." << endl;
+			cout << "input invalido, insira 's' para recomecar, 'n' para sair e 'e' para ver as estatisticas dessa sessao de jogo." << endl;
 			goto tryAgain02;
 		}
 	}
@@ -167,10 +207,12 @@ retryAposta:
 	{
 		goto resume01;
 	}
+	
+
     // nota: sistema de manipulacao de balance ja esta funcionando :DD
-	// 
 	// 2do: (scraped!) fazer verificacao de valor de aposta para que a balance nunca fique menor do que 1
 	// 2do 2: (DONE!) caso a balance fique menor do que 1, terminar o jogo
+	// 2do 3: (DONE!) implementar sistema de estatisticas no final do jogo
 
 	return 0;
 }
